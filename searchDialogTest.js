9e5e6d75e1d57dc621fbae898467d6c4b9049baa
Tucker Gordon <tucker.gordon18@gmail.com>
2016-11-23 08:52:25 -0500

    started working on searchable list

---
+++ searchDialogTest.js
@@ -0,0 +1,10 @@
+var options = {
+	item: 'flora'
+};
+
+var values = [
+	{ sciName: 'flora one', comName: 'f1'}
+	, { sciName: 'flora two', comName: 'f2'}
+];
+
+var floraList = new List('floraList', options, values);
