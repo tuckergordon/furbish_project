9b8fa21e50d85cdd86d9c44beb0e4fa18fdc1039
Tucker Gordon <tucker.gordon18@gmail.com>
2016-11-24 10:30:00 -0500

    initial search of species working

--- searchDialogTest.js
+++ searchDialogTest.js
@@ -1,10 +1,51 @@
-var options = {
-	item: 'flora'
-};
+function makeSciNameList() {
+	// Create the list element:
+	var list = document.createElement('ul');
+	list.setAttribute("id", "sciList");
 
-var values = [
-	{ sciName: 'flora one', comName: 'f1'}
-	, { sciName: 'flora two', comName: 'f2'}
-];
+	var currFirstLetter = "";
 
-var floraList = new List('floraList', options, values);
+	for (var i = 0; i < dataset.length; i++) {
+
+		var floraObject = dataset[i];
+
+		// if (floraObject.sciName[0] != currFirstLetter) {
+		// 	currFirstLetter = floraObject.sciName[0].toUpperCase();
+		// 	var header = document.createElement('li');
+		// 	header.setAttribute("class", "header");
+		// 	header.appendChild(document.createTextNode(currFirstLetter));
+		// 	list.appendChild(header);
+		// }
+		// Create the list item
+		var flora = document.createElement('li');
+
+		// Set its contents:
+		flora.appendChild(document.createTextNode(dataset[i].sciName));
+
+		// Add it to the list
+		list.appendChild(flora);
+	}
+	return list;
+}
+
+// http://www.w3schools.com/howto/howto_js_filter_lists.asp
+function searchFlora() {
+    var input, filter, ul, li, p, i;
+
+    input = document.getElementById("sciSearchBar");
+    filter = input.value.toUpperCase();
+
+    ul = document.getElementById("sciList");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        p = li[i].textContent;
+        if (p.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+}
+
+$("#sciSearchBar").after(makeSciNameList());
